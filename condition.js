8d500a580f63c063cfb795bd88614d1ade27f4c2
Ahmad B <ahmadbello2011@gmail.com>
2022-11-15 21:47:22 -0800

    conditionalstatemt

---
+++ condition.js
@@ -0,0 +1,17 @@
+let firstName = "Olu"
+
+if(firstName =='Olu'){
+    document.getElementById('comma').innerHTML="Information and Communicating Technology ";
+}
+else if(firstName =='Aremu'){
+    document.getElementById('mark').innerHTML=" He is  tallented in programming";
+
+}else if(firstName =='Adekunle'){
+    document.getElementById('ant').innerHTML="knowledge is the key ";
+}
+else if(firstName=='Ade'){
+    document.getElementById('logic').innerHTML="Web programming is an advanced knowledge";
+}
+else{
+    document.getElementById('Kay').innerHTML="Invalid Details";
+}
